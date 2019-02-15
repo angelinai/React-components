1db52a75f33e540a99785ccce11ff0d943b01357
OPEN-TECHS\ailieva <ailieva@open-techs.com>
2019-02-15 15:12:05 -0700

    Add stateless header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {PropTypes} from 'react';
-
+import Header from "./common/Header"
 
 class App extends React.Component {
     render(){
         return(
             <div>
-
+                <Header />
+                
                 {this.props.children}
-
+               
             </div>
         );
     }
