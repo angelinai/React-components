c963ba2240c3d89a4b2566ad049ecd4fa22c87fa
OPEN-TECHS\ailieva <ailieva@open-techs.com>
2019-02-15 15:17:27 -0700

    Add CoursePage and updated routing and Header component

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+// container component
+import React from 'react';
+
+class CoursesPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Courses</h1>
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
