527459185b271b0972569a0931fd40c0a1a43305
OPEN-TECHS\ailieva <ailieva@open-techs.com>
2019-02-19 11:15:53 -0700

    enhance Courses page, introducing state in the course component

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -2,13 +2,13 @@ import React from 'react';
  
 
 class AboutPage extends React.Component {
-    render(){
-        return (
-            <div>  
-                <h1> About </h1>
-                <p> React, Redux and react router </p> 
-             </div>
-        ); 
+
+    render() {
+        return(
+            <div>
+                <h1>About Page </h1>
+            </div>
+        );
     }
 }
 
